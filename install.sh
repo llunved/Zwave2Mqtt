6828a09394b6bf88e9b970e43b30072086b995a4
Daniel Riek <riek@llunved.net>
2020-04-02 23:24:16 -0400

    Basic working instance

--- install.sh
+++ install.sh
@@ -14,15 +14,18 @@ if [ ! -d /host/etc -o ! -d /host/proc -o ! -d /host/var/run ]; then
 fi
 
 # Make sure that we have required directories in the host
-for CUR_DIR in /host/$LOGDIR /host/$DATADIR; do
-    CUR_DIR=${CUR_DIR}/${NAME}
+for CUR_DIR in /host/${LOGDIR}/${NAME} /host/${DATADIR}/${NAME} /host/${DATADIR}/${NAME}/zwave2mqtt /host/${DATADIR}/${NAME}/openzwave  ; do
     if [ -n $CUR_DIR ]; then
         mkdir -p $CUR_DIR
+	if [ "$CUR_DIR" == "/host/${DATADIR}/${NAME}/openzwave" ] ; then
+	    cp -Rv /etc/openzwave.default /host/${DATADIR}/${NAME}/openzwave
+	fi
         chmod -R 775 $CUR_DIR
-	chgrp 0 $CUR_DIR
+	chgrp -R 0 $CUR_DIR
     fi
 done    
 
-chroot /host /usr/bin/podman create --name ${NAME} -p 8091:8091 --device /dev/ttyACM0 --entrypoint /sbin/entrypoint.sh -v ${DATADIR}/${NAME}:/zwave2mqtt/store -v ${LOGDIR}/${NAME}:/var/log/zwave2mqtt ${IMAGE} /bin/start.sh
+
+chroot /host /usr/bin/podman create --name ${NAME} -p 8091:8091 --net=host --device /dev/ttyACM0 --entrypoint /sbin/entrypoint.sh -v ${DATADIR}/${NAME}/zwave2mqtt:/zwave2mqtt/store -v ${DATADIR}/${NAME}/openzwave:/etc/openzwave -v ${LOGDIR}/${NAME}:/var/log/zwave2mqtt ${IMAGE} /bin/start.sh
 chroot /host sh -c "/usr/bin/podman generate systemd --restart-policy=always -t 1 ${NAME} > /etc/systemd/system/zwave2mqtt.service"
 
