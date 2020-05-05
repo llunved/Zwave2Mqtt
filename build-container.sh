605f5f9e38804898f17a03063618ed3b6758d15b
Daniel Riek <riek@llnvd.io>
2020-05-05 09:43:58 -0400

    Upgrading to F32

--- build-container.sh
+++ build-container.sh
@@ -2,18 +2,20 @@
 
 set +x
 
-FEDORA_RELEASE=${FEDORA_RELEASE:-$(grep VERSION_ID /etc/os-release | cut -d '=' -f 2)}
-FEDORA_IMAGE_RT=${FEDORA_IMAGE_BUILD:-"registry.fedoraproject.org/fedora"}
-FEDORA_IMAGE_RT=${FEDORA_IMAGE_RT:-"registry.fedoraproject.org/fedora-minimal"}
+OS_RELEASE=${OS_RELEASE:-$(grep VERSION_ID /etc/os-release | cut -d '=' -f 2)}
+OS_IMAGE=${OS_IMAGE:-"registry.fedoraproject.org/fedora"}
 BUILD_ID=${BUILD_ID:-`date +%s`}
+BUILD_ARCH=${BUILD_ARCH:-`uname -m`}
 PUSHREG=${PUSHREG:-""}
 
-echo sudo podman build --build-arg FEDORA_RELEASE=${FEDORA_RELEASE} --build-arg FEDORA_IMAGE_BUILD=${FEDORA_IMAGE_BUILD} --build-arg FEDORA_IMAGE_RT=${FEDORA_IMAGE_RT} -t ${BUILD_ARCH}/zwave2mqtt:${BUILD_ID} -f Dockerfile.Fedora
-sudo podman build --build-arg FEDORA_RELEASE=${FEDORA_RELEASE} --build-arg FEDORA_IMAGE_RT=${FEDORA_IMAGE_RT} -t ${BUILD_ARCH}/zwave2mqtt:${BUILD_ID} -f Dockerfile.Fedora
-echo sudo  podman tag ${BUILD_ARCH}/zwave2mqtt:${BUILD_ID} ${BUILD_ARCH}/zwave2mqtt:latest
-sudo podman tag ${BUILD_ARCH}/zwave2mqtt:${BUILD_ID} ${BUILD_ARCH}/zwave2mqtt:latest
+echo sudo podman build --build-arg OS_RELEASE=${OS_RELEASE} --build-arg OS_IMAGE=${OS_IMAGE} -t ${BUILD_ARCH}/zwave2mqtt:${BUILD_ID} -f Dockerfile.Fedora
+sudo podman build --build-arg OS_RELEASE=${OS_RELEASE} --build-arg OS_IMAGE=${OS_IMAGE} -t ${BUILD_ARCH}/zwave2mqtt:${BUILD_ID} -f Dockerfile.Fedora
+
 
 if [ $? -eq 0 ]; then
+  echo sudo  podman tag ${BUILD_ARCH}/zwave2mqtt:${BUILD_ID} ${BUILD_ARCH}/zwave2mqtt:latest
+  sudo podman tag ${BUILD_ARCH}/zwave2mqtt:${BUILD_ID} ${BUILD_ARCH}/zwave2mqtt:latest
+
   if [ ! -z "${PUSHREG}" ]; then
     echo sudo podman tag ${BUILD_ARCH}/zwave2mqtt:${BUILD_ID} ${PUSHREG}/${BUILD_ARCH}/zwave2mqtt:${BUILD_ID}
     sudo podman tag ${BUILD_ARCH}/zwave2mqtt:${BUILD_ID} ${PUSHREG}/${BUILD_ARCH}/zwave2mqtt:${BUILD_ID}
