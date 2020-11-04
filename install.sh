aeeeab8d51f292572c3b1f1811f1fff13bfa3baa
Daniel Riek <riek@llnvd.io>
2020-11-04 17:14:05 -0500

    Fixing permissions

--- install.sh
+++ install.sh
@@ -18,14 +18,14 @@ for CUR_DIR in /host/${LOGDIR}/${NAME} /host/${DATADIR}/${NAME} /host/${DATADIR}
     if [ -n $CUR_DIR ]; then
         mkdir -p $CUR_DIR
 	if [ "$CUR_DIR" == "/host/${DATADIR}/${NAME}/openzwave" ] ; then
-	    cp -Rv /etc/openzwave.default /host/${DATADIR}/${NAME}/openzwave
+	    cp -Rv /etc/openzwave.default/ /host/${DATADIR}/${NAME}/openzwave/
 	fi
-        chmod -R 775 $CUR_DIR
+        chmod 775 $CUR_DIR
 	chgrp -R 0 $CUR_DIR
     fi
 done    
 
 
-chroot /host /usr/bin/podman create --name ${NAME} -p 8091:8091 --net=host --device /dev/ttyACM0 --entrypoint /sbin/entrypoint.sh -v ${DATADIR}/${NAME}/zwave2mqtt:/zwave2mqtt/store -v ${DATADIR}/${NAME}/openzwave:/etc/openzwave -v ${LOGDIR}/${NAME}:/var/log/zwave2mqtt ${IMAGE} /bin/start.sh
+chroot /host /usr/bin/podman create --name ${NAME} -p 8091:8091 --net=host --device /dev/ttyACM0:rw --entrypoint /sbin/entrypoint.sh -v ${DATADIR}/${NAME}/zwave2mqtt:/zwave2mqtt/store:rw,z -v ${DATADIR}/${NAME}/openzwave:/etc/openzwave:rw,z -v ${LOGDIR}/${NAME}:/var/log/zwave2mqtt:rw,z ${IMAGE} /bin/start.sh
 chroot /host sh -c "/usr/bin/podman generate systemd --restart-policy=always -t 1 ${NAME} > /etc/systemd/system/${NAME}.service && systemctl daemon-reload && systemctl enable ${NAME}"
 
